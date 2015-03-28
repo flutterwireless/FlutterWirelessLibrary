9579cd8307419c096c7d9f747a521d4d2bf21c66
Taylor Alexander <tlalexander@gmail.com>
2015-03-28 01:15:01 -0700

    Changed license from "LGPL 2.1 or later" to "AGPL 3.0 or later"

--- src/CC1200_Flutter_reg_config_915mhz_1000ksps.h
+++ src/CC1200_Flutter_reg_config_915mhz_1000ksps.h
@@ -1,3 +1,24 @@
+/*
+  Flutter is a lightweight wireless library for embedded systems.
+  Copyright (c) 2015 Flutter Wireless. All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program. If not, the license is also available here:
+  http://www.gnu.org/licenses/
+*/
+
+
+
 // Packet bit length = 0 
 // Whitening = true 
 // Packet length = 255 
@@ -6,7 +27,7 @@
 // Manchester enable = false 
 // Symbol rate = 500 
 // Deviation = 399.169922 
-// Address config = No address check 
+// Address config = Address check on
 // Bit rate = 1000 
 // Modulation format = 4-GFSK 
 // Packet length mode = Variable 
